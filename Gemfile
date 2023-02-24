68a8764b7247deb40a78c7896e719dfb9c32a2e7
Taisho <dadadadadisek@gmail.com>
2023-02-24 22:59:26 +0900

    recommit

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ group :test do
 end
 
 gem 'simple_calendar', '~> 2.0'
+
+gem 'jquery-rails'
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-//= require jquery
+//= require jquery3
 //= require jquery_ujs
-//= require turbolinks
-//= require bootstrap
+//= require_tree .
